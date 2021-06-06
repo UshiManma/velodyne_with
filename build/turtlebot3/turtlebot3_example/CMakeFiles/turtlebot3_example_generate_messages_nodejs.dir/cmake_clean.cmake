a4fc7f02e4067b61692902634b08fd62005c3445
ushiyama <amat.u.41@gmail.com>
2021-06-06 20:54:51 +0900

    1st commit

---
+++ build/turtlebot3/turtlebot3_example/CMakeFiles/turtlebot3_example_generate_messages_nodejs.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/turtlebot3_example_generate_messages_nodejs"
+  "/home/ushiyama/catkin_ws/devel/share/gennodejs/ros/turtlebot3_example/msg/Turtlebot3Goal.js"
+  "/home/ushiyama/catkin_ws/devel/share/gennodejs/ros/turtlebot3_example/msg/Turtlebot3Result.js"
+  "/home/ushiyama/catkin_ws/devel/share/gennodejs/ros/turtlebot3_example/msg/Turtlebot3Action.js"
+  "/home/ushiyama/catkin_ws/devel/share/gennodejs/ros/turtlebot3_example/msg/Turtlebot3Feedback.js"
+  "/home/ushiyama/catkin_ws/devel/share/gennodejs/ros/turtlebot3_example/msg/Turtlebot3ActionGoal.js"
+  "/home/ushiyama/catkin_ws/devel/share/gennodejs/ros/turtlebot3_example/msg/Turtlebot3ActionFeedback.js"
+  "/home/ushiyama/catkin_ws/devel/share/gennodejs/ros/turtlebot3_example/msg/Turtlebot3ActionResult.js"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/turtlebot3_example_generate_messages_nodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ devel/share/turtlebot3_msgs/cmake/turtlebot3_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(turtlebot3_msgs_MESSAGE_FILES "/home/ushiyama/catkin_ws/src/turtlebot3_msgs/msg/SensorState.msg;/home/ushiyama/catkin_ws/src/turtlebot3_msgs/msg/VersionInfo.msg;/home/ushiyama/catkin_ws/src/turtlebot3_msgs/msg/Sound.msg")
+set(turtlebot3_msgs_SERVICE_FILES "")
